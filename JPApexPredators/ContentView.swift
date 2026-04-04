acd6dc4902009eb8ceb118092cd12d2b0d9804f9
sud0pacman <mukhammad.kammoliddin@gmail.com>
2026-04-04 18:57:46 +0500

    Initial Commit

---
+++ JPApexPredators/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JPApexPredators
+//
+//  Created by G'aniyev Muhammad on 04/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by G'aniyev Muhammad on 04/04/26.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
