c7af80d2b5bd816db64cf953cf4266cca08be91b
sud0pacman <mukhammad.kammoliddin@gmail.com>
2026-04-04 23:35:02 +0500

    - Preview modifiers - ScrollView - GeometryReader - ScaleEffect - Offset

---
+++ JPApexPredators/PredatorDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredators
+//
+//  Created by G'aniyev Muhammad on 04/04/26.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorDetail()
+}
