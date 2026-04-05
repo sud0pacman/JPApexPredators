beef6a928fc3a564c847746bf67e8ef2a6192580
sud0pacman <mukhammad.kammoliddin@gmail.com>
2026-04-05 22:23:28 +0500

    - More about position - Map styles - Satallite

---
+++ JPApexPredators/PredatorMap.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorMap.swift
+//  JPApexPredators
+//
+//  Created by G'aniyev Muhammad on 05/04/26.
+//
+
+import SwiftUI
+
+struct PredatorMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorMap()
+}
