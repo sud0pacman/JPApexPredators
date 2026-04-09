3785c58d0183a9ac605d5e90ffc477404e7f7a9a
sud0pacman <mukhammad.kammoliddin@gmail.com>
2026-04-10 00:39:31 +0500

    Add PredatorInfoItem view with placeholder content

---
+++ JPApexPredators/PredatorInfoItem.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorInfoItem.swift
+//  JPApexPredators
+//
+//  Created by G'aniyev Muhammad on 09/04/26.
+//
+
+import SwiftUI
+
+struct PredatorInfoItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorInfoItem()
+}
